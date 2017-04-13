4c7b4ef6411722e33201ab5f1730975c8becfb7b
385841539 <385841539@qq.com>
2017-04-13 19:22:38 +0800

    效果图

--- app/src/main/java/com/example/moreimage/MainActivity.java
+++ app/src/main/java/com/example/moreimage/MainActivity.java
@@ -47,6 +47,7 @@ public class MainActivity extends Activity {
     private long systemTime1;
     private long systemTime2;
     private GridView gridView;
+    private View pb;
 
 
     @Override
@@ -56,6 +57,7 @@ public class MainActivity extends Activity {
         mRxPermissions = new RxPermissions(this);
         calendar = Calendar.getInstance();
         bt = (Button) findViewById(R.id.query);
+        pb = findViewById(R.id.pb);
         gridView = ((GridView) findViewById(R.id.ryMain));
         bt.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -66,14 +68,15 @@ public class MainActivity extends Activity {
                             public void call(Permission permission) {
 
                                 if (permission.granted) {
-                                    // 打开相机，
-                                    small();
+                                    // 打开相机拍照
+                                    takeOnCamera();
 
                                 } else if (permission.shouldShowRequestPermissionRationale) {
                                     //拒绝
-                                    Toast.makeText(MainActivity.this, "您拒绝了打开相机的权限，无法完成", Toast.LENGTH_SHORT);
+                                    Toast.makeText(MainActivity.this, "您拒绝了打开相机的权限，无法完成", Toast.LENGTH_SHORT).show();
                                 } else {
                                     // gotoSetting();
+                                    Toast.makeText(MainActivity.this, "您拒绝了打开相机的权限，无法完成", Toast.LENGTH_SHORT).show();
                                 }
                             }
                         });
@@ -84,34 +87,66 @@ public class MainActivity extends Activity {
 
     }
 
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        Log.e("data", "onActivityResult: " + data);
+        //关闭相机之后获得时间；2；
+        pb.setVisibility(View.VISIBLE);
+        systemTime2 = getSystemTime();
+
+        if (requestCode == REQUEST_SMALL) {
+            //这里可以拓展不同按钮，给下面的方法传不同的参数
+            getContactList();
+        }
+
+        super.onActivityResult(requestCode, resultCode, data);
+    }
+
 
     private void getContactList() {
         mRxPermissions.requestEach(Manifest.permission.READ_EXTERNAL_STORAGE)
                 .subscribe(new Action1<Permission>() {
                     @Override
                     public void call(Permission permission) {
-
                         if (permission.granted) {
-                            //  savePicture();
-
-                            String[] projection = {MediaStore.Images.Media._ID,
+                            //  读取照片然后选择合适的照片保存再list里面
+                            final String[] projection = {MediaStore.Images.Media._ID,
                                     MediaStore.Images.Media.DISPLAY_NAME,
                                     MediaStore.Images.Media.DATA};
-                            String orderBy = MediaStore.Images.Media.DISPLAY_NAME;
-                            Uri uri = MediaStore.Images.Media.EXTERNAL_CONTENT_URI;
-                            List<MyBitmap> list2 = getContentProvider(uri, projection, orderBy);//抽取接口到时候
-                            Log.e("list", "call: " + list2.toString() + ".size" + list2.size());
-                            if (list2 != null) {
-                                if (list2.size() > 7) {
-                                    list2 = list2.subList(list2.size() - 7, list2.size());
+                            final String orderBy = MediaStore.Images.Media.DISPLAY_NAME;
+                            final Uri uri = MediaStore.Images.Media.EXTERNAL_CONTENT_URI;
+                            new Thread(new Runnable() {
+                                @Override
+                                public void run() {
+                                    List<MyBitmap> list2 = getContentProvider(uri, projection, orderBy);//到时候抽取接口
+                                    Log.e("list", "call: " + list2.toString() + ".size" + list2.size());
+                                    if (list2 != null) {
+                                        if (list2.size() > 7) {//这里看要求最多几张照片
+                                            list2 = list2.subList(list2.size() - 7, list2.size());
+                                        }
+                                        final List<MyBitmap> finalList = list2;
+                                        gridView.post(new Runnable() {
+                                            @Override
+                                            public void run() {
+                                                //TODO 拿到数据源然后拓展
+                                                gridView.setAdapter(new Myadapter(MainActivity.this, finalList));//拿到了就可以搞了
+                                                pb.setVisibility(View.GONE);
+
+                                            }
+                                        });
+
+                                    }
+
                                 }
-                                gridView.setAdapter(new Myadapter(MainActivity.this, list2));
-                            }
+                            }).start();
+
                         } else if (permission.shouldShowRequestPermissionRationale) {
                             //拒绝
-                            Toast.makeText(MainActivity.this, "您拒绝了读取照片的权限", Toast.LENGTH_SHORT);
+                            Toast.makeText(MainActivity.this, "您拒绝了读取照片的权限", Toast.LENGTH_SHORT).show();
+
                         } else {
                             // gotoSetting();
+                            Toast.makeText(MainActivity.this, "您拒绝了读取照片的权限", Toast.LENGTH_SHORT).show();
                         }
                     }
                 });
@@ -149,33 +184,24 @@ public class MainActivity extends Activity {
                         String hen = ss.substring(12, 13);
                         if (substring.equals("IMG_") && hen.equals("_")) {
                             String laststring = ss.substring(4, 19).replace("_", "");
-
                             try {
                                 long time = Long.valueOf(laststring).longValue();
                                 if (time > systemTime1 && time <= systemTime2) {
-
                                     set.add(string);
-
                                 }
                             } catch (Exception e) {
                                 Log.e("exception", "getContentProvider: " + e.toString());
-
                             }
-
                         }
-
                     }
-
                 }
-
             }
         }
 
         for (String strings : set) {
-
             Log.e("setsize", "getContentProvider: " + strings);
             try {
-                Bitmap bitmap = convertToBitmap(strings,300,300);
+                Bitmap bitmap = convertToBitmap(strings, 300, 300);
 
                 MyBitmap myBitmap = new MyBitmap(strings, bitmap);
                 lists.add(myBitmap);
@@ -189,29 +215,29 @@ public class MainActivity extends Activity {
         return lists;
     }
 
-    public void small() {
+    public void takeOnCamera() {
         //打开相机之前，记录时间1
         systemTime1 = getSystemTime();
-
         Intent intent = new Intent();
+        //此处之所以诸多try catch，是因为各大厂商手机不确定哪个方法
+        try {
+            intent.setAction(MediaStore.INTENT_ACTION_STILL_IMAGE_CAMERA);
+            startActivityForResult(intent, REQUEST_SMALL);
+        } catch (Exception e) {
+            try {
+                intent.setAction(MediaStore.ACTION_IMAGE_CAPTURE_SECURE);
+                startActivityForResult(intent, REQUEST_SMALL);
 
-        intent.setAction(MediaStore.INTENT_ACTION_STILL_IMAGE_CAMERA);
-
-        startActivityForResult(intent, REQUEST_SMALL);
-    }
-
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        Log.e("data", "onActivityResult: " + data);
-        //关闭相机之后获得时间；2；
-        systemTime2 = getSystemTime();
-
-        if (requestCode == REQUEST_SMALL) {
-            //
-            getContactList();
+            } catch (Exception e1) {
+                try {
+                    intent.setAction(MediaStore.INTENT_ACTION_STILL_IMAGE_CAMERA_SECURE);
+                    startActivityForResult(intent, REQUEST_SMALL);
+                } catch (Exception ell) {
+                    Toast.makeText(MainActivity.this, "请从相册选择", Toast.LENGTH_SHORT).show();
+                }
+            }
         }
 
-        super.onActivityResult(requestCode, resultCode, data);
     }
 
     public long getSystemTime() {
@@ -235,10 +261,14 @@ public class MainActivity extends Activity {
         return timeint;
     }
 
+    /**
+     * 根据路径，二次采样并且压缩
+     * @param filePath 路径
+     * @param destWidth 压缩到的宽度
+     * @param destHeight 压缩到的高度
+     * @return
+     */
     public Bitmap convertToBitmap(String filePath, int destWidth, int destHeight) {
-
-
-
         //第一采样
         BitmapFactory.Options options = new BitmapFactory.Options();
         options.inJustDecodeBounds = true;
@@ -255,7 +285,6 @@ public class MainActivity extends Activity {
         options.inSampleSize = sampleSize;
         options.inPreferredConfig = Bitmap.Config.RGB_565;
         return BitmapFactory.decodeFile(filePath, options);
-
     }
 
 }
--- app/src/main/java/com/example/moreimage/MyBitmap.java
+++ app/src/main/java/com/example/moreimage/MyBitmap.java
@@ -13,6 +13,16 @@ public class MyBitmap {
     public MyBitmap(String path, Bitmap bm) {
         this.path = path;
         this.bm = bm;
+
+//
+//        E/setsize: getContentProvider: /storage/emulated/0/DCIM/Camera/IMG_20170413_165655.jpg
+//        E/setsize: getContentProvider: /storage/emulated/0/DCIM/Camera/IMG_20170413_165652.jpg
+//        E/setsize: getContentProvider: /storage/emulated/0/DCIM/Camera/IMG_20170413_165641.jpg
+//        E/setsize: getContentProvider: /storage/emulated/0/DCIM/Camera/IMG_20170413_165647.jpg
+//        E/setsize: getContentProvider: /storage/emulated/0/DCIM/Camera/IMG_20170413_165643.jpg
+//        E/setsize: getContentProvider: /storage/emulated/0/DCIM/Camera/IMG_20170413_165645.jpg
+//        E/setsize: getContentProvider: /storage/emulated/0/DCIM/Camera/IMG_20170413_165650.jpg
+//        E/setsize: getContentProvider: /storage/emulated/0/DCIM/Camera/IMG_20170413_165659.jpg
     }
 
     public String getPath() {
